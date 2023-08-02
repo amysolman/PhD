ab43a350b91d0c333a93158d5fd6097ec02324d7
Amy Solman <a.b.t.solman@qmul.ac.uk>
2023-08-02 14:57:03 +0100

    Updated data prep scripts

---
+++ 2023-Community-assembly-mechanisms-shaping-snow-ice-and-cryoconite-communities-of-a-High-Arctic-glacier/code/data-prep.R
@@ -0,0 +1,245 @@
+#1. Clear workspace and load packages
+
+rm(list=ls())
+graphics.off()
+
+library(tidyverse)
+library(phyloseq)
+library(readr)
+library(seqinr)
+library(decontam)
+library(vegan)
+library(magrittr)
+library(data.table)
+library(ape) #appe::Ntip
+library(dplyr)
+library(RColorBrewer) #for plotting colours
+library(tidyr) #wide to long format
+library(microbiome)
+
+#2. Import data
+
+metadata <- read.csv(file="../data/metadata.csv", sep=",") #Metadata
+
+#18S rRNA Gene Amplicon Data
+euk_count_tab <- read_tsv(file="../qiime-export/18S/table.tsv", skip=1) #Count Table
+euk_count_tab <- column_to_rownames(euk_count_tab, var = colnames(euk_count_tab)[1]) #first col in our count tab is ASV IDs
+euk_tax <- read_tsv(file="../qiime-export/18S/taxonomy.tsv") #Taxonomy table
+euk_fasta <- read.fasta(file="../qiime-export/18S/dna-sequences.fasta") #Sequences
+euk_tree <- read_tree("../qiime-export/18S/tree.nwk") #Phylogenetic tree
+names(euk_count_tab) = metadata$SampleID #match sample names in count table and metadata
+
+#16S rRNA Gene Amplicon Data
+pro_count_tab <- read_tsv(file="../qiime-export/16S/table.tsv", skip=1) #Count Table
+pro_count_tab <- column_to_rownames(pro_count_tab, var = colnames(pro_count_tab)[1]) #first col in our count tab is ASV IDs
+pro_tax <- read_tsv(file="../qiime-export/16S/taxonomy.tsv") #Taxonomy table
+pro_fasta <- read.fasta(file="../qiime-export/16S/dna-sequences.fasta") #Sequences
+pro_tree <- read_tree("../qiime-export/16S/tree.nwk") #Phylogenetic tree
+names(pro_count_tab) = metadata$SampleID #match sample names in count table and metadata
+
+
+#3.Remove sample S21.76 - this sample we contaminated during library preparation
+
+#from metadata
+metadata = metadata[!metadata$SampleID == "S21.76",]
+
+#16S 
+#asv table
+pro_count_tab = pro_count_tab[,!names(pro_count_tab) == "S21.76"]
+#remove any ASVs that now have zero counts
+pro_count_tab = pro_count_tab[rowSums(pro_count_tab) > 0,]
+#tab table
+pro_tax = pro_tax[pro_tax$`Feature ID` %in% rownames(pro_count_tab),]
+#tree
+pro_tree <- prune_taxa(pro_count_tab$`#OTU ID`, pro_tree)
+#fasta
+pro_fasta = pro_fasta[rownames(pro_count_tab)]
+
+
+#18S
+#asv table
+euk_count_tab = euk_count_tab[,!names(euk_count_tab) == "S21.76"]
+#remove any ASVs that now have zero counts
+euk_count_tab = euk_count_tab[rowSums(euk_count_tab) > 0,]
+#tax table
+euk_tax = euk_tax[euk_tax$`Feature ID` %in% rownames(euk_count_tab),]
+#tree
+euk_tree <- prune_taxa(euk_count_tab$`#OTU ID`, euk_tree)
+#fasta
+euk_fasta = euk_fasta[rownames(euk_count_tab)]
+
+#4. Sort metadata
+
+#18S rRNA Gene Data
+euk_meta = metadata[! names(metadata) %in% c("SampleID_16S")] #remove unneccesary data from the metadata file
+rownames(euk_meta) = euk_meta$SampleID #make sure row has the right names
+
+#16S rRNA Gene Data
+pro_meta = metadata[! names(metadata) %in% c("SampleID_18S")] #remove unneccesary data from the metadata file
+rownames(pro_meta) = pro_meta$SampleID #make sure row has the right names
+
+
+#5. Sort taxonomy table - we want our Qiime taxonomy output to be in a form appropriate for building a phyloseq object
+
+#16S
+pro_taxa_tab <- pro_tax %>%
+  mutate(pro_tax=str_replace_all(string=Taxon, pattern=".__", replacement="")) %>%
+  mutate(pro_tax=str_replace_all(string=pro_tax, pattern=";$", replacement="")) %>%
+  mutate(pro_tax=str_replace_all(string=pro_tax, pattern=" ", replacement="")) %>%
+  separate(pro_tax, into=c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"), sep=";") %>%
+  dplyr::select(-Taxon, -Confidence) %>%
+  column_to_rownames(var='Feature ID')
+
+#18S
+euk_taxa_tab <- euk_tax %>%
+  mutate(euk_tax=str_replace_all(string=Taxon, pattern=".__", replacement="")) %>%
+  mutate(euk_tax=str_replace_all(string=euk_tax, pattern=";$", replacement="")) %>%
+  mutate(euk_tax=str_replace_all(string=euk_tax, pattern=" ", replacement="")) %>%
+  separate(euk_tax, into=c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"), sep=";") %>%
+  dplyr::select(-Taxon, -Confidence) %>%
+  column_to_rownames(var='Feature ID')
+
+
+#6. Output number of sequences per sample in raw reads
+
+#16S
+pro_count_tab_df = as.data.frame(colSums(pro_count_tab)) #make into df
+pro_count_tab_df$Sample = rownames(pro_count_tab_df) #make sample names a column
+names(pro_count_tab_df) = c("Reads", "Sample")
+
+pro_p<-ggplot(data=pro_count_tab_df, aes(x=Sample, y=Reads)) +
+  geom_bar(stat="identity")+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+pro_p
+
+#18S
+euk_count_tab_df = as.data.frame(colSums(euk_count_tab)) #make into df
+euk_count_tab_df$Sample = rownames(euk_count_tab_df) #make sample names a column
+names(euk_count_tab_df) = c("Reads", "Sample")
+
+euk_p<-ggplot(data=euk_count_tab_df, aes(x=Sample, y=Reads)) +
+  geom_bar(stat="identity")+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+euk_p
+
+
+#7. Raw data basic stats: total reads per dataset, number of unique ASVs
+
+#18S
+euk_tot = sum(euk_count_tab_df$Reads) #total reads in the datazset
+euk_asv = nrow(euk_count_tab) #number of unique ASVs
+
+pro_tot = sum(pro_count_tab_df$Reads) #total reads in the datazset
+pro_asv = nrow(pro_count_tab) #number of unique ASVs
+
+
+#8. Convert data into phyloseq objects
+
+#16S
+pro_ASV = otu_table(as.matrix(pro_count_tab), taxa_are_rows = TRUE)
+pro_TAX = tax_table(as.matrix(pro_taxa_tab))
+pro_META = sample_data(data.frame(pro_meta, row.names = rownames(pro_meta)))
+pro_TREE = pro_tree
+pro_ps <- phyloseq(pro_ASV, pro_TAX, pro_META, pro_TREE) #make into phyloseq object
+
+#18S
+euk_ASV = otu_table(as.matrix(euk_count_tab), taxa_are_rows = TRUE) 
+euk_TAX = tax_table(as.matrix(euk_taxa_tab))
+euk_META = sample_data(data.frame(euk_meta, row.names = rownames(euk_meta)))
+euk_TREE = euk_tree
+euk_ps <- phyloseq(euk_ASV, euk_TAX, euk_META, euk_TREE) #make into phyloseq object
+
+
+#9. Remove ambiguous annotations and singletons
+
+#16S
+pro_ps.ambig <- subset_taxa(pro_ps, !is.na(Kingdom) & !Kingdom %in% c("Unassigned", "Eukaryota") & !Order %in% c("Chloroplast") & !Family %in% c("Mitochondria"))
+pro_ps.ambig <- prune_taxa(taxa_sums(pro_ps.ambig) >= 10, pro_ps.ambig)
+
+#eukaryote (18S) dataset with micrometazoan's removed.
+euk_ps.ambig <- subset_taxa(euk_ps, !is.na(Kingdom) & !Kingdom %in% c("Unassigned", "Bacteria", "Archaea") & !Phylum %in% c("Vertebrata", "Tardigrada", "Rotifera", "Nematozoa") & !Class %in% c("Embryophyta"))
+euk_ps.ambig <- prune_taxa(taxa_sums(euk_ps.ambig) >= 10, euk_ps.ambig) 
+
+#total eukaryote (18S) dataset
+euk_ps.tot <- subset_taxa(euk_ps, !is.na(Kingdom) & !Kingdom %in% c("Unassigned", "Bacteria", "Archaea") & !Phylum %in% c("Vertebrata") & !Class %in% c("Embryophyta"))
+euk_ps.tot <- prune_taxa(taxa_sums(euk_ps.tot) >= 10, euk_ps.tot) 
+
+#make a micrometazoa phyloseq object
+ps.mm <- subset_taxa(euk_ps, !is.na(Kingdom) & !Kingdom %in% c("Unassigned", "Bacteria", "Archaea") & Phylum %in% c("Tardigrada", "Rotifera", "Nematozoa") & !Class %in% c("Embryophyta"))
+ps.mm <- prune_taxa(taxa_sums(ps.mm) >= 10, ps.mm) 
+
+
+#10. Re-root tree. Use this function > (https://john-quensen.com/r/unifrac-and-tree-roots/) < picks longest branch
+
+# First define the function from link above to find furthest outgroup
+pick_new_outgroup <- function(tree.unrooted){
+  #tablif parts of the tree that we need
+  treeDT <- 
+    cbind(
+      data.table(tree.unrooted$edge),
+      data.table(length = tree.unrooted$edge.length)
+    )[1:Ntip(tree.unrooted)] %>%
+    cbind(data.table(id = tree.unrooted$tip.label))
+  #Take out the longest terminal branch as outgroup
+  new.outgroup <- treeDT[which.max(length)]$id
+  return(new.outgroup)
+}
+
+# Run on my_phyloseq tree
+#16S
+pro_tree_new <- phy_tree(pro_ps.ambig)
+pro_new_outgroup <- pick_new_outgroup(pro_tree_new)
+pro_new_tree_root <- ape::root(pro_tree_new, outgroup=pro_new_outgroup, resolve.root=TRUE) # Re-root tree
+pro_new_tree_dich <- ape::multi2di(pro_new_tree_root) # Convert to dichotomy tree
+phy_tree(pro_ps.ambig) <- pro_new_tree_dich
+
+#18S without micro
+euk_tree_new <- phy_tree(euk_ps.ambig)
+euk_new_outgroup <- pick_new_outgroup(euk_tree_new)
+euk_new_tree_root <- ape::root(euk_tree_new, outgroup=euk_new_outgroup, resolve.root=TRUE) # Re-root tree
+euk_new_tree_dich <- ape::multi2di(euk_new_tree_root) # Convert to dichotomy tree
+phy_tree(euk_ps.ambig) <- euk_new_tree_dich
+
+#18S with micro
+euk_tree_new.tot <- phy_tree(euk_ps.tot)
+euk_new_outgroup.tot <- pick_new_outgroup(euk_tree_new.tot)
+euk_new_tree_root.tot <- ape::root(euk_tree_new.tot, outgroup=euk_new_outgroup.tot, resolve.root=TRUE) # Re-root tree
+euk_new_tree_dich.tot <- ape::multi2di(euk_new_tree_root.tot) # Convert to dichotomy tree
+phy_tree(euk_ps.tot) <- euk_new_tree_dich.tot
+
+#18S micrometazoans only
+ps.mm_tree_new <- phy_tree(ps.mm)
+ps.mm_outgroup <- pick_new_outgroup(ps.mm_tree_new)
+ps.mm_root <- ape::root(ps.mm_tree_new, outgroup=ps.mm_outgroup, resolve.root=TRUE) # Re-root tree
+ps.mm_dich <- ape::multi2di(ps.mm_root) # Convert to dichotomy tree
+phy_tree(ps.mm) <- ps.mm_dich
+
+
+#11. Filtered data basic stats: total reads per dataset, number of unique ASVs
+
+#16S
+filt_pro_tot = sum(colSums(data.frame(otu_table(pro_ps.ambig))))
+filt_pro_asv = nrow(data.frame(otu_table(pro_ps.ambig)))
+
+#18S without micrometazoans
+filt_euk_tot = sum(colSums(data.frame(otu_table(euk_ps.ambig))))
+filt_euk_asv = nrow(data.frame(otu_table(euk_ps.ambig)))
+
+#all 18S
+filt_euk_tot.tot = sum(colSums(data.frame(otu_table(euk_ps.tot))))
+filt_euk_asv.tot = nrow(data.frame(otu_table(euk_ps.tot)))
+
+#micrometazoans
+filt_ps.mm_tot = sum(colSums(data.frame(otu_table(ps.mm))))
+filt_ps.mm_asv = nrow(data.frame(otu_table(ps.mm)))
+
+
+#12. Save phyloseq objects
+saveRDS(pro_ps.ambig, "../results/16S-phylo-object.rds")
+saveRDS(euk_ps.ambig, "../results/18S-phylo-object-micro-remove.rds")
+saveRDS(euk_ps.tot, "../results/18S-phylo-object-micro-keep.rds")
+saveRDS(ps.mm, "../results/18S-phylo-object-micro-only.rds")
---
+++ 2023-Divergent-assembly-mechanisms-of-rare-and-abundant-microbial-communities-in-cryoconite-holes-on-Arctic-and-Antarctic-glaciers/code/data-prep.R
@@ -0,0 +1,317 @@
+#1. Clear workspace and load packages
+
+
+rm(list=ls())
+graphics.off()
+
+library(tidyverse)
+library(phyloseq)
+library(readr)
+library(seqinr)
+#BiocManager::install("decontam")
+library(decontam)
+library(vegan)
+library(magrittr)
+library(data.table)
+library(ape) #appe::Ntip
+library(dplyr)
+
+
+#2. Import data
+
+
+#16S
+
+#Count Table
+pro_count_table <- read_tsv(file="../qiime-export/16S/table.tsv", skip=1)
+
+#Specify the first column in our count table is the row names (ASV IDs)
+pro_count_table <- column_to_rownames(pro_count_table, var = colnames(pro_count_table)[1])
+pro_sample_names = as.numeric(names(pro_count_table))
+
+#Rest of the data
+pro_tax <- read_tsv(file="../qiime-export/16S/taxonomy.tsv")
+pro_fasta <- read.fasta(file="../qiime-export/16S/dna-sequences.fasta")
+pro_tree <- read_tree("../qiime-export/16S/tree.nwk")
+
+#18S
+
+#Count Table
+euk_count_table <- read_tsv(file="../qiime-export/18S/table.tsv", skip=1)
+
+#Specify the first column in our count table is the row names (ASV IDs)
+euk_count_table <- column_to_rownames(euk_count_table, var = colnames(euk_count_table)[1])
+#sample_names = as.numeric(names(count_table))
+
+#Rest of the data
+euk_tax <- read_tsv(file="../qiime-export/18S/taxonomy.tsv")
+euk_fasta <- read.fasta(file="../qiime-export/18S/dna-sequences.fasta")
+euk_tree <- read_tree("../qiime-export/18S/tree.nwk")
+
+#Metadata
+metadata <- read.csv(file="../data/metadata.csv", sep=",")
+
+#3. Wrangle metadata
+#Sort metadata and count dataframes so they have the same sample names in the same order
+
+#16S
+#remove unnecessary data from the metadata file
+pro_metadata = metadata[! names(metadata) %in% c("SampleID_18S")]
+#rename columns
+names(pro_metadata)[names(pro_metadata) == 'SampleID_16S'] <- 'SampleID'
+#make sure row has the right names
+rownames(pro_metadata) = pro_metadata$SampleID
+
+#18S
+#remove unnecessary data from the metadata file
+euk_metadata = metadata[! names(metadata) %in% c("SampleID_16S")]
+#rename columns
+names(euk_metadata)[names(euk_metadata) == 'SampleID_18S'] <- 'SampleID'
+#make sure row has the right names
+rownames(euk_metadata) = euk_metadata$SampleID
+
+#4. Sort taxonomy table so our output from QIIME classification step is useful for analysis
+
+#16S
+pro_taxa_tab <- pro_tax %>%
+  mutate(pro_tax=str_replace_all(string=Taxon, pattern=".__", replacement="")) %>%
+  mutate(pro_tax=str_replace_all(string=pro_tax, pattern=";$", replacement="")) %>%
+  mutate(pro_tax=str_replace_all(string=pro_tax, pattern=" ", replacement="")) %>%
+  separate(pro_tax, into=c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"), sep=";") %>%
+  dplyr::select(-Taxon, -Confidence) %>%
+  column_to_rownames(var='Feature ID')
+
+#18S
+euk_taxa_tab <- euk_tax %>%
+  mutate(euk_tax=str_replace_all(string=Taxon, pattern=".__", replacement="")) %>%
+  mutate(euk_tax=str_replace_all(string=euk_tax, pattern=";$", replacement="")) %>%
+  mutate(euk_tax=str_replace_all(string=euk_tax, pattern=" ", replacement="")) %>%
+  separate(euk_tax, into=c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"), sep=";") %>%
+  dplyr::select(-Taxon, -Confidence) %>%
+  column_to_rownames(var='Feature ID')
+
+
+#5. Output number of sequences per sample in raw reads
+
+#16S
+pro_samp.counts = as.data.frame(colSums(pro_count_table))
+pro_samp.counts$Sample = rownames(pro_samp.counts)
+names(pro_samp.counts) = c("Reads", "Sample")
+
+pro_p<-ggplot(data=pro_samp.counts, aes(x=Sample, y=Reads)) +
+  geom_bar(stat="identity")+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+pro_p
+
+#18S
+euk_samp.counts = as.data.frame(colSums(euk_count_table))
+euk_samp.counts$Sample = rownames(euk_samp.counts)
+names(euk_samp.counts) = c("Reads", "Sample")
+
+euk_p<-ggplot(data=euk_samp.counts, aes(x=Sample, y=Reads)) +
+  geom_bar(stat="identity")+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+euk_p
+
+
+#6. Raw data basic stats: total reads per dataset, number of unique ASVs
+
+#16S
+sum(pro_samp.counts$Reads) #reads
+nrow(pro_count_table) #ASVs
+
+#18S
+sum(euk_samp.counts$Reads) #reads
+nrow(euk_count_table) #ASVs
+
+
+#7. Identify contaminants + print as table
+
+#16S
+
+# convert count table to dataframe
+count_table_df <- as.data.frame(pro_count_table)
+#what is the sample ID of the Negative Control sample
+pro_neg.samp = pro_metadata[pro_metadata$Name == "NegCtrlBlank16",]
+# A logical vector showing which entries are 'TRUE' for controls and 'FALSE' for genuine samples
+pro_decontam <- startsWith(names(count_table_df), rownames(pro_neg.samp))
+# Run isContaminant, while transforming the matrix with t()
+contam_df <- isContaminant(t(count_table_df), neg=pro_decontam)
+# What do our results look like?
+table(contam_df$contaminant) 
+# Make a vector holding contaminant IDs
+pro_contam_asvs <- row.names(contam_df[contam_df$contaminant == TRUE, ])
+# Check the taxonomy of these contaminants
+pro_contam_tab <- pro_taxa_tab[rownames(pro_taxa_tab) %in% pro_contam_asvs, ]
+write.csv(pro_contam_tab, "../results/16S-contaminant-asvs.csv")
+
+
+#18S
+
+# convert count table to dataframe
+count_table_df <- as.data.frame(euk_count_table)
+#what is the sample ID of the Negative Control sample
+euk_neg.samp = euk_metadata[euk_metadata$Name == "NegCtrlBlank16",]
+# A logical vector showing which entries are 'TRUE' for controls and 'FALSE' for genuine samples
+euk_decontam <- startsWith(names(count_table_df), rownames(euk_neg.samp))
+# Run isContaminant, while transforming the matrix with t()
+contam_df <- isContaminant(t(count_table_df), neg=euk_decontam)
+# What do our results look like?
+table(contam_df$contaminant) 
+# Make a vector holding contaminant IDs
+euk_contam_asvs <- row.names(contam_df[contam_df$contaminant == TRUE, ])
+# Check the taxonomy of these contaminants
+euk_contam_tab <- euk_taxa_tab[rownames(euk_taxa_tab) %in% euk_contam_asvs, ]
+write.csv(euk_contam_tab, "../results/18S-contaminant-asvs.csv")
+
+#8. Remove contaminants from data objects
+
+#16S
+#COUNT TABLE
+x <- rownames(pro_count_table) %in% pro_contam_asvs #which rows are contaminants?
+pro_count_table <- pro_count_table[!x,] #remove those rows
+
+#what is the index of "TRUE" values in decontam?
+true_index <- NULL
+for (i in 1:length(pro_decontam)){
+  if (pro_decontam[[i]] == "TRUE"){
+    true_index <- c(true_index, i)
+  }
+}
+pro_count_table <- pro_count_table[,-c(true_index)]  #remove those samples
+
+#METADATA
+row_names_df_to_remove<-c(rownames(pro_neg.samp))
+pro_metadata = pro_metadata[!(row.names(pro_metadata) %in% row_names_df_to_remove),]
+
+###TAXONOMY FILE
+pro_taxonomy <- pro_taxa_tab[ ! rownames(pro_taxa_tab) %in% pro_contam_asvs, ]
+# 
+###FASTA FILE
+#Remove contaminants from dna sequences file
+pro_fasta = pro_fasta[pro_contam_asvs] <- NULL
+
+###PHYLOGENETIC TREE
+# This is a phyloseq object so we can use a phyloseq command called prune_taxa
+pro_tree <- prune_taxa(pro_count_table$`#OTU ID`, pro_tree)
+
+
+#18S
+#COUNT TABLE
+x <- rownames(euk_count_table) %in% euk_contam_asvs #which rows are contaminants?
+euk_count_table <- euk_count_table[!x,] #remove those rows
+
+#what is the index of "TRUE" values in decontam?
+true_index <- NULL
+for (i in 1:length(euk_decontam)){
+  if (euk_decontam[[i]] == "TRUE"){
+    true_index <- c(true_index, i)
+  }
+}
+euk_count_table <- euk_count_table[,-c(true_index)]  #remove those samples
+
+#METADATA
+row_names_df_to_remove<-c(rownames(euk_neg.samp))
+euk_metadata = euk_metadata[!(row.names(euk_metadata) %in% row_names_df_to_remove),]
+
+###TAXONOMY FILE
+euk_taxonomy <- euk_taxa_tab[ ! rownames(euk_taxa_tab) %in% euk_contam_asvs, ]
+# 
+###FASTA FILE
+#Remove contaminants from dna sequences file
+euk_fasta = euk_fasta[euk_contam_asvs] <- NULL
+
+###PHYLOGENETIC TREE
+# This is a phyloseq object so we can use a phyloseq command called prune_taxa
+euk_tree <- prune_taxa(euk_count_table$`#OTU ID`, euk_tree)
+
+
+#9. Convert data into phyloseq objects
+
+#16S
+pro_ASV = otu_table(as.matrix(pro_count_table), taxa_are_rows = TRUE)
+pro_TAX = tax_table(as.matrix(pro_taxonomy))
+pro_META = sample_data(data.frame(pro_metadata, row.names = rownames(pro_metadata)))
+pro_TREE = pro_tree
+
+#make into phyloseq object
+pro_ps <- phyloseq(pro_ASV, pro_TAX, pro_META, pro_TREE)
+
+#18S
+euk_ASV = otu_table(as.matrix(euk_count_table), taxa_are_rows = TRUE)
+euk_TAX = tax_table(as.matrix(euk_taxonomy))
+euk_META = sample_data(data.frame(euk_metadata, row.names = rownames(euk_metadata)))
+euk_TREE = euk_tree
+
+#make into phyloseq object
+euk_ps <- phyloseq(euk_ASV, euk_TAX, euk_META, euk_TREE)
+
+
+#10. Remove ambiguous annotations and singletons
+
+#16S
+pro_ps.ambig <- subset_taxa(pro_ps, !is.na(Kingdom) & !Kingdom %in% c("Unassigned", "Eukayote") & !Order %in% c("Chloroplast") & !Family %in% c("Mitochondria"))
+pro_ps.ambig <- prune_taxa(taxa_sums(pro_ps.ambig) > 1, pro_ps.ambig) 
+
+#18S dataset with micrometazoan's removed
+euk_ps.ambig <- subset_taxa(euk_ps, !is.na(Kingdom) & !Kingdom %in% c("Unassigned", "Bacteria", "Archaea") & !Phylum %in% c("Vertebrata", "Tardigrada", "Rotifera", "Nematozoa") & !Class %in% c("Embryophyta") & !Family %in% c("Mitochondria"))
+euk_ps.ambig <- prune_taxa(taxa_sums(euk_ps.ambig) >= 1, euk_ps.ambig) 
+
+
+#11. Re-root tree. Use this function > (https://john-quensen.com/r/unifrac-and-tree-roots/) < picks longest branch
+
+# First define the function from link above to find furthest outgroup
+pick_new_outgroup <- function(tree.unrooted){
+  #tablif parts of the tree that we need
+  treeDT <- 
+    cbind(
+      data.table(tree.unrooted$edge),
+      data.table(length = tree.unrooted$edge.length)
+    )[1:Ntip(tree.unrooted)] %>%
+    cbind(data.table(id = tree.unrooted$tip.label))
+  #Take out the longest terminal branch as outgroup
+  new.outgroup <- treeDT[which.max(length)]$id
+  return(new.outgroup)
+}
+
+
+#16S
+pro_tree_new <- phy_tree(pro_ps.ambig)
+pro_new_outgroup <- pick_new_outgroup(pro_tree_new)
+# Re-root tree
+pro_new_tree_root <- ape::root(pro_tree_new, outgroup=pro_new_outgroup, resolve.root=TRUE)
+# Convert to dichotomy tree
+pro_new_tree_dich <- ape::multi2di(pro_new_tree_root)
+phy_tree(pro_ps.ambig) <- pro_new_tree_dich
+
+#18S
+euk_tree_new <- phy_tree(euk_ps.ambig)
+euk_new_outgroup <- pick_new_outgroup(euk_tree_new)
+# Re-root tree
+euk_new_tree_root <- ape::root(euk_tree_new, outgroup=euk_new_outgroup, resolve.root=TRUE)
+# Convert to dichotomy tree
+euk_new_tree_dich <- ape::multi2di(euk_new_tree_root)
+phy_tree(euk_ps.ambig) <- euk_new_tree_dich
+
+#12. Filtered data basic stats: total reads per dataset, number of unique ASVs
+
+#16S
+sum(colSums(data.frame(otu_table(pro_ps.ambig)))) #reads
+nrow(data.frame(otu_table(pro_ps.ambig))) #ASVs
+
+#18S
+sum(colSums(data.frame(otu_table(euk_ps.ambig)))) #reads
+nrow(data.frame(otu_table(euk_ps.ambig))) #ASVs
+
+
+#13. Save phyloseq objects
+
+#16S
+saveRDS(pro_ps.ambig, "../results/16S-phylo-object.rds")
+
+#18S
+saveRDS(euk_ps.ambig, "../results/18S-phylo-object.rds")
