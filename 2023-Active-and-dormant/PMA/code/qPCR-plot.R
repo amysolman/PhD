14e2a1f1cad5ff055b1ade813436e84aedbae93f
Amy Solman <a.b.t.solman@qmul.ac.uk>
2023-11-06 14:37:37 +0000

    qPCR analysis scripts update

--- 2023-Active-and-dormant/PMA/code/qPCR-plot.R
+++ 2023-Active-and-dormant/PMA/code/qPCR-plot.R
@@ -9,35 +9,63 @@ library(dplyr)
 
 #load data
 df = read.csv("../data/meta_qpcr_res.csv", sep="\t")
+df2 = read.csv("../data/qPCR-mean.csv", sep="\t")
 
 #set the habitat order
 df$Habitat = factor(df$Habitat, levels=c("Snow", "Spring Ice", "Summer Ice", "Cryoconite", "Control"))
+df2$Habitat = factor(df2$Habitat, levels=c("Snow", "Spring Ice", "Summer Ice", "Cryoconite", "Control"))
 
 #plot
 p = ggplot(df) +
-  geom_point(aes(Sample.Name.To.Plot, log(Gene.Copies.Per.mL.g), color = Treatment, shape = Treatment), size=4) +
+  geom_point(aes(Sample.Name.To.Plot, log(Gene.Copies.Per.mL.g), color = Treatment, shape = Treatment, stroke=2), size=6) +
   ylab("log Gene Copies per mL/g")+
   xlab("Sample")+
   facet_grid(cols = vars(Habitat), scales = "free", space = "free", labeller = label_wrap_gen(width=10))+
-  guides(color=guide_legend(override.aes=list(shape=c(21,24), alpha=.5, size=4)))+
+  guides(color=guide_legend(override.aes=list(shape=c(21,24), size=6, stroke=2)))+
   scale_shape_manual(values = c(21,24))+
   theme_bw()+
   theme(legend.position="bottom",
-        axis.text.x = element_text(angle = 45, vjust = 0.5),
-        legend.title=element_blank())
+        axis.text.x = element_text(angle = 45, vjust = 0.5, size=10),
+        legend.title=element_blank(),
+        legend.text = element_text(size=15),
+        axis.title.x = element_text(size=15),
+        axis.title.y = element_text(size=15),
+        strip.text = element_text(size=15))
 
 #save the plot
 pdf("../results/qPCR-plot.pdf", height=5, width=15)
 print(p)
 dev.off()
 
+#second plot
+p2 = ggplot(df2) +
+  geom_point(aes(Sample, log(Gene.Copies.Per.mL.g), color = Treatment, shape = Treatment, stroke=2), size=6) +
+  ylab("log Gene Copies per mL/g")+
+  xlab("Sample")+
+  facet_grid(cols = vars(Habitat), scales = "free", space = "free", labeller = label_wrap_gen(width=10))+
+  guides(color=guide_legend(override.aes=list(shape=c(21,24), size=6, stroke=2)))+
+  scale_shape_manual(values = c(21,24))+
+  theme_bw()+
+  theme(legend.position="bottom",
+        axis.text.x = element_text(angle = 45, vjust = 0.5, size=10),
+        legend.title=element_blank(),
+        legend.text = element_text(size=15),
+        axis.title.x = element_text(size=15),
+        axis.title.y = element_text(size=15),
+        strip.text = element_text(size=15))
+
+#save the plot
+pdf("../results/qPCR-plot2.pdf", height=5, width=15)
+print(p2)
+dev.off()
+
 #another way to plot paired data
 
 # Subset gene data without treatment
-tdna <- subset(df,  Treatment == "tDNA", Gene.Copies.Per.mL.g,
+tdna <- subset(df2,  Treatment == "tDNA", Gene.Copies.Per.mL.g,
                  drop = TRUE)
 # subset gene data with treatment
-idna <- subset(df,  Treatment == "iDNA", Gene.Copies.Per.mL.g,
+idna <- subset(df2,  Treatment == "iDNA", Gene.Copies.Per.mL.g,
                 drop = TRUE)
 # Plot paired data
 pd <- paired(tdna, idna)
@@ -45,7 +73,7 @@ plot(pd, type = "profile") + theme_bw()
 
 
 #basic stats
-group_by(df, Treatment, Habitat) %>%
+group_by(df2, Treatment, Habitat) %>%
   summarise(
     count = n(),
     mean = mean(Gene.Copies.Per.mL.g, na.rm = TRUE),
@@ -58,7 +86,7 @@ group_by(df, Treatment, Habitat) %>%
 #is the data normally distributed?
 
 # compute the difference
-d <- with(df, 
+d <- with(df2, 
           Gene.Copies.Per.mL.g[Treatment == "tDNA"] - Gene.Copies.Per.mL.g[Treatment == "iDNA"])
 # Shapiro-Wilk normality test for the differences
 shapiro.test(d) # => p-value < 0.05 so the data is not normally distributed
@@ -67,24 +95,50 @@ shapiro.test(d) # => p-value < 0.05 so the data is not normally distributed
 #t.test(Gene.Copies.Per.mL.g ~ Treatment, paired = TRUE, data = df)
 
 #nonparametric paired sample Wilcoxon test
-res <- wilcox.test(Gene.Copies.Per.mL.g ~ Treatment, data = df, paired = TRUE)
-res  
+wilcox.test(Gene.Copies.Per.mL.g ~ Treatment, data = df2, paired = TRUE) #no significant difference between 
 
-#nonparametic paired sample Wilcoxon test (is the median gene number after treatment less than 
-#the median gene number before treatment?)
-res <- wilcox.test(Gene.Copies.Per.mL.g ~ Treatment, data = df, paired = TRUE,
-                   alternative="less")
-res  
 
-#these results suggest that overallthe PMA treated samples had a significantly higher number of gene copies per mL/g sample than PMA un-treated samples
+#nonparametic paired sample Wilcoxon test (is the median gene number in samples treated with PMA (iDNA) 
+#less than untreated samples (tDNA)?)
+# x = df2[df2$Treatment == "iDNA",]
+# y = df2[df2$Treatment == "tDNA",]
+# wilcox.test(x$Gene.Copies.Per.mL.g, y$Gene.Copies.Per.mL.g, paired = TRUE,
+#                    alternative="less")
+
+#nonparametic paired sample Wilcoxon test (is the median gene number in samples treated with PMA (iDNA) 
+#greater than untreated samples (tDNA)?)
+# wilcox.test(x$Gene.Copies.Per.mL.g, y$Gene.Copies.Per.mL.g, paired = TRUE,
+#             alternative="greater")
+
+#these results suggest that overall the PMA treated samples had a significantly higher number of gene copies per mL/g sample than PMA un-treated samples
 
 #what does this look like per group?
-sn <- wilcox.test(Gene.Copies.Per.mL.g ~ Treatment, data = df[df$Habitat == "Snow",], paired = TRUE)
+sn <- wilcox.test(Gene.Copies.Per.mL.g ~ Treatment, data = df2[df2$Habitat == "Snow",], paired = TRUE)
 sn  
-sp <- wilcox.test(Gene.Copies.Per.mL.g ~ Treatment, data = df[df$Habitat == "Spring Ice",], paired = TRUE)
+sp <- wilcox.test(Gene.Copies.Per.mL.g ~ Treatment, data = df2[df2$Habitat == "Spring Ice",], paired = TRUE)
 sp  
-sm <- wilcox.test(Gene.Copies.Per.mL.g ~ Treatment, data = df[df$Habitat == "Summer Ice",], paired = TRUE)
+sm <- wilcox.test(Gene.Copies.Per.mL.g ~ Treatment, data = df2[df2$Habitat == "Summer Ice",], paired = TRUE)
 sm  
-cr <- wilcox.test(Gene.Copies.Per.mL.g ~ Treatment, data = df[df$Habitat == "Cryoconite",], paired = TRUE)
+cr <- wilcox.test(Gene.Copies.Per.mL.g ~ Treatment, data = df2[df2$Habitat == "Cryoconite",], paired = TRUE)
 cr  
 
+#what happens if we test spring and summer samples separately?
+#nonparametric paired sample Wilcoxon test
+wilcox.test(Gene.Copies.Per.mL.g ~ Treatment, data = df[df$Season == "Spring",], paired = TRUE)
+wilcox.test(Gene.Copies.Per.mL.g ~ Treatment, data = df[df$Season == "Summer",], paired = TRUE)
+
+#Spring
+x = df[df$Season == "Spring" & df$Treatment == "iDNA",]
+y = df[df$Season == "Spring" & df$Treatment == "tDNA",]
+wilcox.test(x$Gene.Copies.Per.mL.g, y$Gene.Copies.Per.mL.g, paired = TRUE)
+
+#Summer
+x = df[df$Season == "Summer" & df$Treatment == "iDNA",]
+y = df[df$Season == "Summer" & df$Treatment == "tDNA",]
+wilcox.test(x$Gene.Copies.Per.mL.g, y$Gene.Copies.Per.mL.g, paired = TRUE, alternative = "less")
+
+#test for differences between individual pairs
+data = df[df$Sample == "S21.29",]
+x = data[data$Treatment == "iDNA",]
+y = data[data$Treatment == "tDNA",]
+wilcox.test(x$Gene.Copies.Per.mL.g, y$Gene.Copies.Per.mL.g, paired = TRUE)
